66b9cf786844c62cc861966e8758c5908869e9ab
ThaungThanHan <ericlimmm115@gmail.com>
2021-01-04 15:22:00 +0630

    Auth setup,adminLTE setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserTableSeeder;
+use Database\Seeders\TablesTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([               // calling the existing created seeders
+            UserTableSeeder::class,
+            TablesTableSeeder::class,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DishesController;
+use App\Http\Controllers\OrdersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([          // Auth::routes(); ka default akone pr tal. Ae dr mha m lo tr phyote yin [] nae false htr lite 
+    'register' => false,
+    'reset' => false,
+    'verify' => false,
+    'confirm' => false
+]);
+
+Route::get('/home', [App\Http\Controllers\OrdersController::class, 'index'])->name('home');
+Route::resource('/dishes', App\Http\Controllers\DishesController::class);  // controller tat tat pl so array [] htae syr m loss
